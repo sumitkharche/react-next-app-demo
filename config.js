8e675ff0dedd438f8aed42ba64a902965bbbb6cd
Sumit Kharche <sumit.kharche@yahoo.in>
2021-11-11 23:53:14 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,6 @@
+export default {
+  bucket: {
+    slug: process.env.COSMIC_BUCKET || '951e70b0-4197-11ec-bd99-191f9fcdd080',
+    read_key: process.env.COSMIC_READ_KEY || 'xPWbAa2XliYnYXVPr85dPhoE8iioUvJTO7sEOCsoA0GcTGL2gU'
+  }
+}
